2fb5d319ae7964dcc1039c3c2ca4b2d259345fd6
jhamile08 <jhamiledr222@gmail.com>
2024-03-18 13:19:39 -0500

    jdbc, creacion del connection

---
+++ JDBC/src/database/ConfigDB.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Esta clase se encargara de establecer y cerrar la conexion en la base de datos
+public class ConfigDB {
+    // este atrubuto tendra el estado de la conexion
+    static Connection objConnection = null;
+    // Metodo para conectar la bd
+    public static Connection openConnection(){
+        try {
+            //Llamamos al driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Creamos la variable de conexion
+            String url = "jdbc:mysql://localhost:3306/_01_jdbc_lovelace";
+            String user = "root";
+            String password = "Rlwl2023.";
+            //establecer conexion
+            objConnection =  (Connection) DriverManager.getConnection(url,user,password);
+            System.out.println("Me conecte perfectamente");
+        }catch (ClassNotFoundException error){
+            System.out.println("ERROR >> Driver no instalado" + error.getMessage());
+        }catch (SQLException error){
+            System.out.println("ERROR >> error al conectar la base de datos" + error.getMessage());
+        }
+        return null;
+    }
+
+    public static void closeConnection(){
+        try {
+            // Si hay una conexion actica entonces la cierra
+            if( objConnection != null) objConnection.close();
+            System.out.println("Se finalizo la conexion con exito");
+        }catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}
